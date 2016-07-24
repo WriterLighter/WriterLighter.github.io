e1e4937426ea91b94b2420375e8d9d302599c703
Yugo Yagita <2000ygkn0713@gmail.com>
2016-07-24 10:56:31 +0000

    change coming soon

--- js/script.js
+++
@@ -1,8 +0,0 @@
-(function($) {
-    $(function() {
-        // Nav Toggle Button
-        $('#nav-toggle').click(function(){
-            $("#top-head").toggleClass('open');
-        });
-    });
-})(jQuery);
